7a6559435be86fcc975b81202e59b236e1a7296e
Rohan <rohanzealous@gmail.com>
2021-10-26 10:19:22 -0700

    donezo

--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -6,8 +6,9 @@ public class BankClient {
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your preferred name?");
         String name = scan.nextLine();
+
         // Create a bank account with the given name and random account number
         BankAccount account = new BankAccount(name);
-        System.out.println(account);
+
     }
 }
